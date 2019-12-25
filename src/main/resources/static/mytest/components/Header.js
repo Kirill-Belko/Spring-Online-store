c6a998b069839697e0d63b7c754048152e633a2f
Kirill-Belko <belko291094@yandex.ru>
2019-12-25 18:34:58 +0300

    add react component Header with bootstrap container on main page

--- src/main/resources/static/mytest/components/Header.js
+++ src/main/resources/static/mytest/components/Header.js
@@ -1,3 +1,7 @@
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+//import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDom from "react-dom";
 
@@ -8,7 +12,26 @@ class Header extends React.Component {
 
     render(){
         return (
-            <h1>Hello from Header</h1>
+            <div className="container">
+                <h1>Выберите категорию товаров</h1>
+                <div className="row">
+                    <div className="col-sm">
+                        Телефоны
+                    </div>
+                    <div className="col-sm">
+                        Телевизоры
+                    </div>
+                    <div className="col-sm">
+                        Компьютеры
+                    </div>
+                    <div className="col-sm">
+                        Техника для кухни
+                    </div>
+                    <div className="col-sm">
+                        Техника для дома
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- target/classes/static/mytest/main.js
+++ target/classes/static/mytest/main.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDom from "react-dom";
 import Header from './components/Header.js';
 import {render} from 'react-dom';
 
